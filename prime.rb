62972717efacac4b79f9422434b9e98a8937e514
Andrei Lyskov <andreilyskov@gmail.com>
2017-08-17 00:35:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  counter = 2
+  return false if int == -1 || int == 1 || int == 0
+  while counter < int.abs
+    if int % counter == 0
+      return false
+    end
+    counter += 1
+  end
+  return true
+end
+
+
+puts prime?(3)
